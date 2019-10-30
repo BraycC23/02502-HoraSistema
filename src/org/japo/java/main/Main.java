4c3eba636d6e1ac993ebe4f3b712e786ea446b97
BRAYAN <BRAYAN@192.168.1.111>
2019-10-30 16:35:42 +0100

    Ejercicio 02 Tema 025

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,42 @@
+/* 
+Copyright 2019  Brayan C. Sanchez Arenas - brayancamilo.sanchez.alum2iescap.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Brayan C. Sanchez Arenas
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //Variables
+        int H;
+        int M;
+        int S;
+        // GregorianCalendar
+        GregorianCalendar gc = new GregorianCalendar();
+        
+        // Obtención valores
+        H = gc.get(Calendar.HOUR_OF_DAY);
+        M = gc.get(Calendar.MINUTE);
+        S = gc.get(Calendar.SECOND);
+        //Mensaje 
+        System.out.printf("Hora del sistema ....: %02d:%02d:%02d%n",H,M,S);
+    }
+}
